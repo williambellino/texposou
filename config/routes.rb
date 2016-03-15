c7a600612fde9236bc39a38a92e6c5307d5426f3
AlexandreJolly <accounts@alexandrejolly.com>
2016-03-16 00:44:47 +0100

    Reinstall project with Postgres + Install devise (users)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'page#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
