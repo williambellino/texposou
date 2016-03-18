06944b7d70d87cae2a9b06e5c8056d5048e26d3d
Dagan <costaoverblog@ymail.com>
2016-03-18 11:02:00 +0100

    Added possibility to create new place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
 
   def index
     @places = Place.all.order('created_at DESC')
-    @exhibitions = Exhibition.all.order('created_at DESC')
+    @exhibitions = Exhibition.where(status: 'published').order('created_at DESC')
   end
 
   def about
